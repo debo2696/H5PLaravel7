684473993458445f1dd94e9330d21d546421eb1d
Debajyoti Das <debo2696@gmail.com>
2021-07-06 13:22:52 +0530

    changed env for db migration, migrated db and modified composer classmap and added provider in config/app.php

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        EscolaSoft\LaravelH5p\LaravelH5pServiceProvider::class,
+
     ],
 
     /*
